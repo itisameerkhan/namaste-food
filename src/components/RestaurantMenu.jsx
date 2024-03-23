1e18d45fced73065abefc78a0633b605e3062c07
itisameerkhan <itisameerkhan@gmail.com>
2024-03-23 08:22:33 +0530

    update

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -17,7 +17,6 @@ const RestaurantMenu = () => {
   const fetchMenu = async () => {
     const data = await fetch(MENU_API + resId);
     const json = await data.json();
-    // console.log(json);
     setResInfo(json?.data);
   };
 
