bafbbbd27184efe87e5efc243ba6ee15afc19181
itisameerkhan <itisameerkhan@gmail.com>
2024-03-27 07:47:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/restaurants/:resId",
-        element: <RestaurantMenu />
-      }
+        element: <RestaurantMenu />,
+      },
     ],
   },
 ]);
