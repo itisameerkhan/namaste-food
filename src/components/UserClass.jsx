05e78958c81d00e47c5ce1d380a5649bf6196f2b
itisameerkhan <itisameerkhan@gmail.com>
2024-03-30 22:39:15 +0530

    componentWillUpdate

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,61 +1,83 @@
 import React from "react";
 import "./UserClass.scss";
 
-class ChildClass extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props.name + " constructor called");
-  }
-
-  render() {
-    console.log(this.props.name + " render called");
-
-    return (
-      <h1>class component -> {this.props.name}</h1>
-    )
-  };
-
-  componentDidMount() {
-    console.log(this.props.name + " componentDidMount called");
-  }
-}
-
 class UserClass extends React.Component {
   constructor(props) {
-    console.log("parent class constructor called");
+    console.log("constructor");
     super(props);
 
     this.state = {
+      userInfo: {},
       count: 0,
     };
   }
+
+  // async componentDidMount() {
+  //   console.log("componentDiMount");
+  //   const data = await fetch("https://api.github.com/users/akshaymarch7");
+  //   const json = await data.json();
+
+  //   this.setState({
+  //     userInfo: json,
+  //   });
+  // }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUmount");
+  }
+
   render() {
-    console.log("parent class render called");
+    console.log("render");
+    const { name, avatar_url, bio, public_repos, followers, following } =
+      this.state.userInfo;
+
     const { count } = this.state;
 
     return (
+      // <div className="user-card">
+      //   <img
+      //     src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      //     alt=""
+      //   />
+      //   <div className="pfp">
+      //     <img src={avatar_url} alt="" />
+      //   </div>
+      //   <div className="user-data">
+      //     <p>{name}</p>
+      //     <p>{bio}</p>
+      //   </div>
+      //   <div className="user-data-2">
+      //     <div>
+      //       <span className="material-symbols-outlined">list</span>
+      //       {public_repos}
+      //     </div>
+      //     <div>
+      //       <span className="material-symbols-outlined">group</span>
+      //       {followers}
+      //     </div>
+      //     <div>
+      //       <span className="material-symbols-outlined">person_add</span>
+      //       {following}
+      //     </div>
+      //   </div>
+      //   <div className="follow-btn">
+      //     <button className="follow">Follow</button>
+      //   </div>
+      // </div>
+
       <div className="user-class">
-      <ChildClass name={"First"} />
-      <ChildClass name={"Second"} />
-        {/* <h1>Class based Component</h1>
-        <h2>Count : {count}</h2>
-
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-            console.log("count ->", count);
-          }}
-        >
-          INCREASE
-        </button> */}
-      </div> 
+        <h1>Count: {count}</h1>
+        <button onClick={() => {
+          this.setState({
+            count: this.state.count + 1,
+          })
+        }}>INCREASE</button>
+      </div>
     );
-  }
-
-  componentDidMount() {
-    console.log("parentclass ComponentDidMount Called");
   }
 }
 
